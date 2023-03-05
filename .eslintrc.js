5715c8018cbddcee289c5ee397e1b55e2d330321
Piyush Kacha <piyushkacha75@gmail.com>
2023-03-05 16:35:10 +0530

    feat(boilerplate) huskly and lint related changes added

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,9 +14,13 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
+    // NestJS default rules
     '@typescript-eslint/interface-name-prefix': 'off',
+    // NestJS default rules
     '@typescript-eslint/explicit-function-return-type': 'off',
+    // NestJS default rules
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    // NestJS default rules
     '@typescript-eslint/no-explicit-any': 'off',
     // to avoid line ending issues in windows & linux (LF vs CRLF)
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
@@ -25,5 +29,13 @@ module.exports = {
     curly: ['error', 'all'],
     'no-trailing-spaces': 'error',
     'lines-between-class-members': 'error',
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: ['_id', '_default'],
+      },
+    ],
+    // For Typescript, it is better not to use default export: https://stackoverflow.com/a/33307487/11440474
+    'import/prefer-default-export': 'off',
   },
 };
